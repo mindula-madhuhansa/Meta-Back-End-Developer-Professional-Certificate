27a934ef70e4853cad491c05d417a9d45c0bbbe7
Mindula Madhuhansa <76749525+mindula-madhuhansa@users.noreply.github.com>
2023-08-09 20:48:14 +0530

    add week 2

--- APIs/Week 2/DRF/BookListAPI/views.py
+++ APIs/Week 2/DRF/BookListAPI/views.py
@@ -4,6 +4,6 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def books(request):
     return Response("list of the books", status=status.HTTP_200_OK)
