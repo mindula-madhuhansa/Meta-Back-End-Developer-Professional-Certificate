f9400e14c32acc298191dd81aac520732a61569c
Mindula Madhuhansa <76749525+mindula-madhuhansa@users.noreply.github.com>
2023-08-10 15:24:24 +0530

    complete drf tutorial

--- APIs/Week 2/DRF/BookListAPI/urls.py
+++ APIs/Week 2/DRF/BookListAPI/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.books)
+    # path('books/', views.books),
+    path('books/', views.BookList.as_view()),
+    path('books/<int:pk>', views.Book.as_view()),
 ]
